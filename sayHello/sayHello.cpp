ad7cd62ab385cb5d14571a69f36be5560dc9df20
thohuynh1512 <thohuynh1512@gmail.com>
2019-05-21 23:27:50 +0700

    my project

---
+++ sayHello/sayHello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World";
+	return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
